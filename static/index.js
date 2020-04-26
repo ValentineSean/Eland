5092578838e4d85d7cef1dc570caba6dbc2e75ac
ValentineSean <seanchanengeta@gmail.com>
2020-04-26 23:15:18 +0200

    Eland static pages

---
+++ static/index.js
@@ -0,0 +1,39 @@
+function displayShona(){
+    document.getElementById("shona_name").style.display = "block";
+    document.getElementById("ndebele_name").style.display = "none";
+    document.getElementById("french_name").style.display = "none";
+    document.getElementById("spanish_name").style.display = "none";
+    document.getElementById("description").style.display = "none";
+}
+
+function displayNdebele(){
+    document.getElementById("shona_name").style.display = "none";
+    document.getElementById("ndebele_name").style.display = "block";
+    document.getElementById("french_name").style.display = "none";
+    document.getElementById("spanish_name").style.display = "none";
+    document.getElementById("description").style.display = "none";
+}
+
+function displayFrench(){
+    document.getElementById("shona_name").style.display = "none";
+    document.getElementById("ndebele_name").style.display = "none";
+    document.getElementById("french_name").style.display = "block";
+    document.getElementById("spanish_name").style.display = "none";
+    document.getElementById("description").style.display = "none";
+}
+
+function displaySpanish(){
+    document.getElementById("shona_name").style.display = "none";
+    document.getElementById("ndebele_name").style.display = "none";
+    document.getElementById("french_name").style.display = "none";
+    document.getElementById("spanish_name").style.display = "block";
+    document.getElementById("description").style.display = "none";
+}
+
+function displayDescription(){
+    document.getElementById("shona_name").style.display = "none";
+    document.getElementById("ndebele_name").style.display = "none";
+    document.getElementById("french_name").style.display = "none";
+    document.getElementById("spanish_name").style.display = "none";
+    document.getElementById("description").style.display = "block";
+}
